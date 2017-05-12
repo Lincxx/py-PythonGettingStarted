f7403bce7131a2c3d2b8b5ac960b82599c2570e5
Jeremy Lincoln <bossco28@gmail.com>
2017-05-12 16:55:56 -0400

    Student App

---
+++ 15-StudentApp/studentapp.py
@@ -0,0 +1,67 @@
+# Student app
+
+students = []
+
+
+def get_students_titlecase():
+    students_titlecase = []
+    for student in students:
+        students_titlecase.append(student['name'].title())
+    return students_titlecase
+
+
+def print_students_titlecase():
+    students_titlecase = get_students_titlecase()
+    print(students_titlecase)
+
+
+# A function with a default
+def add_student(name, student_id=332):
+    student = {"name": name, "student_id": student_id}
+    students.append(student)
+
+
+def save_file(student):
+    try:
+        f = open("students.txt", "a")
+        f.write(student + "\n")
+        f.close()
+    except Exception:
+        print("Could not save file")
+
+
+def read_file():
+    try:
+        f = open("students.txt", "r")
+        for student in f.readlines():
+            add_student(student)
+        f.close()
+    except Exception:
+        print("Could not read file")
+
+read_file()
+print_students_titlecase()
+
+student_name = input("Enter in student name: ")
+student_id = input("Enter in student id: ")
+add_student(student_name, student_id)
+save_file(student_name)
+
+
+# add_stu = input("Would you like to add a new student: (Y)es or (N)o: ")
+#
+# if add_stu.title() == 'Y':
+#     adding_student = True
+#     while adding_student:
+#         student_name = input("Enter in student name: ")
+#         student_id = input("Enter in student id: ")
+#         add_student(student_name, student_id)
+#         add_stu = input("Would you like to add a new student: (Y)es or (N)o: ")
+#         if add_stu.title() == 'N':
+#             break
+# else:
+#     print_students_titlecase()
+# #
+# # print(get_students_titlecase())
+# print_students_titlecase()
+
