24e9483d196bf8538d57e26f8fc2623a314ba6f1
Jeremy Lincoln <bossco28@gmail.com>
2017-05-12 08:46:17 -0400

    Boolean and None

---
+++ 4-BoolAndNone/BoolAndNone.py
@@ -0,0 +1,12 @@
+python_course = True
+java_course = False
+
+#convert a bool into an int
+int(python_course) == 1
+int(java_course) == 0
+
+#convert a bool into a string
+str(python_course) == "True"
+
+#None is null in other languages
+aliens_found = None #this could be helpful as a placeholder and return False in an if statement
