a4ce6020043440b857bf44316e5e2ff6c7497ab0
Jeremy Lincoln <bossco28@gmail.com>
2017-05-12 13:42:19 -0400

    Other Data Types

--- 12-OtherDataTypes/OtherDataTypes.py
+++ 12-OtherDataTypes/OtherDataTypes.py
@@ -1,1 +1,13 @@
 # Other Data Types
+
+# Here is a brief overview of the other data types
+
+# complex
+
+# bytes and bytearry range of 0 - 255
+
+# tuples (2,3,'hi') - these are immutable - most common in this list
+
+# set and frozenset with this we can remove any duplicate
+# eg [3,2,1,3,5] now use a set([[3,2,1,3,5]]) will produce (1,2,3,5)
+
