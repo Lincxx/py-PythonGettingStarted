4fa433c5097b93bd46c0bcba8c60fe97a2d3d8be
Jeremy Lincoln <bossco28@gmail.com>
2017-06-05 08:30:40 -0400

    generator, yield and lambda functions

---
+++ 15-StudentApp/lambda.py
@@ -0,0 +1,9 @@
+def double1(x):
+    return x * 2
+
+
+#lambda
+double2 = lambda x: x * 2
+
+print(double1(2))
+print(double2(2))
---
+++ 15-StudentApp/yield.py
@@ -0,0 +1,19 @@
+students = []
+
+def read_file():
+    try:
+        f = open("students.txt")
+        for student in read_students(f):
+            students.append(student)
+        f.close()
+    except Exception:
+        print("Could not read file")
+
+
+#generator function
+def read_students(f):
+    for line in f:
+        yield line
+
+read_file()
+print(students)
